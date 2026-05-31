7f4d63d129ebd4c74a33ef0a99f8bfde1e09a487
Leolie1225 <103999549+Leolie1225@users.noreply.github.com>
2026-05-31 09:19:01 +0800

    Change success message in upload_data function
    Updated success message for data upload in upload_data function.

--- exam_board/views.py
+++ exam_board/views.py
@@ -4,27 +4,24 @@ from django.shortcuts import render
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import ExamData  # 引入ExamData 模型
+from .models import ExamData  
 
 
 @csrf_exempt
 def upload_data(request):
-    """功能一：数据上传（支持同时存入标题和考试列表）"""
     if request.method == 'POST':
         try:
-            # 解析前端传过来的 JSON 数据
             data = json.loads(request.body)
             date = data.get("exam_date", "")
             title = data.get('exam_title', '')
             ex_list = data.get('exam_list', '')
 
-            # 往数据库创建一条新记录
             ExamData.objects.create(
                 exam_date=date,
                 exam_title=title,
                 exam_list=ex_list
             )
-            return JsonResponse({"status": "success", "message": "考试看板数据保存成功！"})
+            return JsonResponse({"status": "success", "message": "考试数据保存成功！"})
         except Exception as e:
             return JsonResponse({"status": "error", "message": str(e)})
 
@@ -32,8 +29,6 @@ def upload_data(request):
 
 
 def read_data(request):
-    """功能二：数据读取（获取最新的一条考试数据）"""
-    # 获取数据库里最新创建的一条数据
     last_exam = ExamData.objects.last()
 
     if last_exam:
